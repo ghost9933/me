ddf69a6c87b671859be268de464b1a678adf4773
ghost9933 <mhatrenikhil36@gmail.com>
2024-06-07 00:18:54 -0500

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: '', // Replace '<repository-name>' with your GitHub repository name
+  assetPrefix: '',
+
+  images: {
+    loader: 'imgix', // Use 'imgix' as the loader
+    path: '', // Specify an empty path to use relative URLs for image loading
+    unoptimized: true // Disable optimization to avoid server-side processing
+  }
+};
 
 export default nextConfig;
